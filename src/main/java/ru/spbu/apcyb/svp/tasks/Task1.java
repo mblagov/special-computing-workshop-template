eebb624a4f32e6d217751d37f77aa12ad3b9c49c
astatochek <71458498+astatochek@users.noreply.github.com>
2022-09-20 19:30:45 +0300

    Task 1 code and tests added

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
@@ -1,11 +1,81 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Задание 1.
  */
 public class Task1 {
 
-  public static void main(String[] args) {
+  public static void recursion(
+      List<ArrayList<Integer>> combinations,
+      List<Integer> values,
+      int currentSum,
+      int valueIndex,
+      List<Integer> combination
+  ) {
+    if (currentSum == 0) {
+      combinations.add((ArrayList<Integer>) combination);
+    } else if (valueIndex < values.size()) {
+      for (int i = 0; i * values.get(valueIndex) <= currentSum; i++) {
+        ArrayList<Integer> nextCombination = new ArrayList<>(combination);
+        for (int j = 0; j < i; j++) {
+          nextCombination.add(values.get(valueIndex));
+        }
+        recursion(
+            combinations,
+            values,
+            currentSum - i * values.get(valueIndex),
+            valueIndex + 1,
+            nextCombination
+        );
+      }
+    }
+  }
 
+  public static List<ArrayList<Integer>> getAllCombinations(int[] input) {
+    int sum = input[0];
+    ArrayList<Integer> values = new ArrayList<>();
+    for (int i = 1; i < input.length; i++) {
+      values.add(input[i]);
+    }
+
+    Collections.sort(values);
+    ArrayList<ArrayList<Integer>> combinations = new ArrayList<>();
+
+    recursion(
+        combinations,
+        values,
+        sum,
+        0,
+        new ArrayList<>()
+    );
+
+    if (combinations.size() == 1 && combinations.get(0).isEmpty()) {
+      return new ArrayList<>();
+    }
+
+    return combinations;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt)
+        .toArray();
+
+    List<ArrayList<Integer>> combinations = getAllCombinations(input);
+
+    Logger logger = Logger.getLogger(Task1.class.getName());
+
+    logger.log(Level.INFO, () -> String.valueOf(combinations.size()));
+    for (ArrayList<Integer> combination : combinations) {
+      logger.log(Level.INFO, () -> combination.toString().replaceAll("[^\\p{N}\\s]+", ""));
+    }
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -1,8 +1,45 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 /**
  * Тесты для задания 1.
  */
 public class Task1Test {
 
+  @Test
+  void getAllCombinationsTest1() {
+    ArrayList<ArrayList<Integer>> expected = new ArrayList<>(
+        Arrays.asList(
+            new ArrayList<>(Arrays.asList(2, 2)),
+            new ArrayList<>(Arrays.asList(1, 1, 2)),
+            new ArrayList<>(Arrays.asList(1, 1, 1, 1))
+        )
+    );
+    ArrayList<ArrayList<Integer>> actual = (ArrayList<ArrayList<Integer>>) Task1.getAllCombinations(
+        new int[]{4, 2, 1});
+    Assertions.assertEquals(expected, actual);
+  }
+
+
+  @Test
+  void getAllCombinationsTest2() {
+    ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> actual = (ArrayList<ArrayList<Integer>>) Task1.getAllCombinations(
+        new int[]{0, 9, 8, 7, 6, 5, 4, 3, 2, 1});
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void getAllCombinationsTest3() {
+    ArrayList<ArrayList<Integer>> expected = new ArrayList<>();
+    ArrayList<ArrayList<Integer>> actual = (ArrayList<ArrayList<Integer>>) Task1.getAllCombinations(
+        new int[]{4, 9, 8, 7, 6, 5});
+    Assertions.assertEquals(expected, actual);
+  }
 }
+
