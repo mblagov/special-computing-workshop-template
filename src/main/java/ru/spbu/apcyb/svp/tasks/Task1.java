18e689a79fff2e552764751605e1ad516e86f4b4
maija <mayyaandreevna@yandex.ru>
2022-10-13 10:51:13 +0300

    Все работает Но покрытие 66

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
@@ -11,7 +11,6 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //List<String> banknotes = new ArrayList<>();
-
         System.out.print("input the amount: ");
         int amount = in.nextInt();
 
@@ -28,35 +27,42 @@ public class Task1 {
 
         System.out.print("----------------------\n");
 
+
+        //сделать норм тесты
+        //сделать норм вывод
+        //как обработать исключения?
+        //при нуле выдает ошибку и при отриц значениях тоже
+        //тест на обычные числа
+        //тест на большую купюру
+
+
+        List<List<Integer>> result = exchangeOfBanknotes(amount, banknotes);
+        System.out.printf("number of combinations:  %d\n", result.size());
+        if (result.size() != 0) {
+            System.out.print("combinations:\n");
+            for (var combination : result) {
+                for (int banknote : combination) {
+                    System.out.printf("%d ", banknote);
+                }
+                System.out.println();
+            }
+        } else { System.out.print("Sorry, not today"); }
+        in.close();
+
+    }
+
+    public static List<List<Integer>> exchangeOfBanknotes(int amount, int[] banknotes) {
+        banknotes = Arrays.stream(banknotes).distinct().toArray(); //дубликаты убирает
         int maxBanknote = -1;
         for (int num : banknotes) { //а как иначе найти максимум?
             if (maxBanknote < num) {
                 maxBanknote = num;
             }
         }
-        List<List<Integer>> result = exchangeOfBanknotes(amount, maxBanknote, banknotes);
-        System.out.print("number of combinations: ");
-        System.out.println(result.size());
-        if (result.size() != 0) {
-            System.out.print("combinations:\n");
-            for (var combination : result) {
-                for (int banknote : combination) {
-                    System.out.print(banknote);
-                    System.out.print(" ");
-                }
-                System.out.print("\n");
-            }
-        } else {
-            System.out.print("Sorry, not today");
-        }
-        in.close();
 
-    }
-
-    public static List<List<Integer>> exchangeOfBanknotes(int amount, int minvalue, int[] banknotes) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> valueList = new ArrayList<>();
-        return exchangeOfBanknotes(amount, minvalue, banknotes, valueList, result);
+        return exchangeOfBanknotes(amount, maxBanknote, banknotes, valueList, result);
     }
 
     private static List<List<Integer>> exchangeOfBanknotes(int amount, int minvalue, int[] banknotes, List<Integer> valueList, List<List<Integer>> result) {
@@ -81,6 +87,4 @@ public class Task1 {
             exchangeOfBanknotes(amount - b, b, banknotes, copyValueList, result);
 
         }
-        return result;
-    }
-}
+        return result;}}
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -1,18 +1,55 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import org.eclipse.sisu.containers.InjectedTestCase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
+import ru.spbu.apcyb.svp.tasks.Task1;
+
 
 /**
  * Тесты для задания 1.
  */
 public class Task1Test {
-    @Test //ярлычок для идеи)
-    void isEmpty(){
-        exchangeOfBanknotes(90, 20, List.of(20));
+    @Test
+        //ярлычок для идеи)
+    void isEmpty() {
+        List<List<Integer>> result = Task1.exchangeOfBanknotes(90,  new int[]{20});
+        List<List<Integer>> answer = new ArrayList<>();
+        Assertions.assertIterableEquals(result, answer);
     }
 
+    @Test
+    void isRepeatedBanknotes() {
+        List<List<Integer>> result = Task1.exchangeOfBanknotes(20,  new int[]{10, 10});
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> answerInt = new ArrayList<>();
+        answerInt.add(10);
+        answerInt.add(10);
+        answer.add(answerInt);
+        Assertions.assertIterableEquals(result, answer);
+    }
 
+    @Test
+    void isBanknotes() {
+        List<List<Integer>> result = Task1.exchangeOfBanknotes(20,  new int[]{10});
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> answerInt = new ArrayList<>();
+        answerInt.add(10);
+        answerInt.add(10);
+        answer.add(answerInt);
+        Assertions.assertIterableEquals(result, answer);
+    }
+
+    @Test
+    void isBigBanknotes() {
+        List<List<Integer>> result = Task1.exchangeOfBanknotes(20,  new int[]{30});
+        List<List<Integer>> answer = new ArrayList<>();
+        Assertions.assertIterableEquals(result, answer);
+    }
 }
