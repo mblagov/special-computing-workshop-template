61aa69d871fc982a18248a922d0343340c6ed936
PAiratEe <airatsafin65578@gmail.com>
2022-10-13 10:56:00 +0300

    Task 3 v.1

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,38 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
 
+  @Test
+  void testEmptyInput() throws IOException {
+    String[] args = new String[2];
+    args[0] = "";
+    args[1] = "answer.txt";
+    try (FileWriter writer = new FileWriter(args[1], false)) {
+      Task3.walk(args[0], writer, 0);
+    } catch (FileNotFoundException thrown) {
+      assertEquals("java.io.FileNotFoundException", thrown.toString());
+    }
+  }
+
+  @Test
+  void testInput() throws IOException {
+    String[] args = new String[2];
+    args[0] = "src/main";
+    args[1] = "answer.txt";
+    try (FileWriter writer = new FileWriter(args[1], false)) {
+      Task3.walk(args[0], writer, 0);
+    } catch (FileNotFoundException thrown) {
+      assertEquals("java.io.FileNotFoundException", thrown.toString());
+    }
+  }
 }
