1107574c5ab55ddf6a7098b37328d496a56c8286
user <st085727@student.spbu.ru>
2022-10-22 11:13:01 +0300

    Task 3

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,36 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
+
+    @Test
+    void test2() throws IOException {
+        FileWriter resFile = new FileWriter("RESULT.txt", false);
+        FileNotFoundException thrown =
+                assertThrows(FileNotFoundException.class, () -> Task3.getFileTree( resFile, "123"));
+        assertEquals("java.io.FileNotFoundException", thrown.toString());
+    }
+
+    @Test
+    void test1() throws IOException {
+        FileWriter resFile = new FileWriter("RESULT.txt", false);
+        boolean thrown = true;
+        try {
+            Task3.getFileTree( resFile, "..//");
+        } catch (IOException e) {
+            thrown = false;
+        }
+        assertTrue(thrown);
+    }
 
 }
