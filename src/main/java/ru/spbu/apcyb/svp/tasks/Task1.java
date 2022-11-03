774397411574892c2568aa7a98471626429673d9
artemiytoropchin <artemiy.toropchin@gmail.com>
2022-11-03 10:38:22 +0300

    first try

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
@@ -1,11 +1,77 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Задание 1.
  */
 public class Task1 {
+  /*
+   * finds all ways to exchange passed value with passed coins
+   * params: total -- value to exchange; int
+   *         coinsStr -- exchange values; list of strings
+   * returns: solutions in next format "coin:number of coins"; list of hashmaps
+   */
+  static List<HashMap<Integer, Integer>> findSolutions(int total, List<String> coinsStr) {
+    List<Integer> coins = new ArrayList<>();
+    coinsStr.forEach(coin -> coins.add(Integer.parseInt(coin)));
+    List<HashMap<Integer, Integer>> resList = new ArrayList<>();
+    HashMap<Integer, Integer> start = new HashMap<>();
+    coins.forEach(coin -> start.put(coin, 0));
 
+    recursionSolver(total, coins, 0, start, resList);
+
+    return resList;
+  }
+
+  private static  HashMap<Integer, Integer> recursionSolver(int total, List<Integer> coins,
+      int curVal, HashMap<Integer, Integer> solution, List<HashMap<Integer, Integer>> resList) {
+    if (total < 0) {
+      return new HashMap<>();
+    } else if (total == 0) {
+      return solution;
+    } else {
+      for (Integer coin : coins) {
+        if (coin >= curVal) {
+          HashMap<Integer, Integer> newSolution = new HashMap<>(solution);
+          newSolution.put(coin, newSolution.get(coin) + 1);
+          HashMap<Integer, Integer> unfilteredSolution = recursionSolver(total - coin, coins,
+              coin, newSolution, resList);
+          if (!unfilteredSolution.isEmpty()) {
+            resList.add(unfilteredSolution);
+          }
+        }
+      }
+    }
+    return new HashMap<>();
+  }
+
+  /*
+   * params: total -- value to exchange
+   *         coins -- exchange values
+   * returns: print total number of solutions and solutions in next format "coin:number of coins".
+   */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+    int total = scanner.nextInt();
+    scanner.nextLine();
+    List<String> coinsStr = List.of(scanner.nextLine().split("\\s+"));
+
+    scanner.close();
+
+    List<HashMap<Integer, Integer>> resList = findSolutions(total, coinsStr);
+
+    System.out.println(resList.size());
+    for (HashMap<Integer, Integer> res : resList) {
+      for (var entry : res.entrySet()) {
+        System.err.print(entry.getKey() + ":" + entry.getValue() + " ");
+      }
+      System.err.println();
+    }
   }
 }
+
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -1,8 +1,95 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 1.
  */
-public class Task1Test {
+class Task1Test {
+  @Test
+  void emptyCoins() {
+    List<HashMap<Integer, Integer>> calculatedAns = Task1.findSolutions(90, new ArrayList<>());
+    List<HashMap<Integer, Integer>> trueAns = new ArrayList<>();
+    Assertions.assertIterableEquals(calculatedAns, trueAns);
+  }
 
+  @Test
+  void incorrectCoins() {
+    boolean exception = false;
+    try {
+      Task1.findSolutions(10, Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+    } catch (Exception failRun) {
+      exception = true;
+    }
+    Assertions.assertTrue(exception);
+  }
+
+  @Test
+  void correctRun() {
+    boolean exception = false;
+    try {
+      Task1.findSolutions(10, Arrays.asList("1", "2", "5"));
+    } catch (Exception failRun) {
+      exception = true;
+    }
+    Assertions.assertFalse(exception);
+  }
+
+  @Test
+  void correctAnswer1() {
+    List<HashMap<Integer, Integer>> calculatedAns =
+        Task1.findSolutions(5, Arrays.asList("2", "3"));
+    List<HashMap<Integer, Integer>> trueAns = new ArrayList<>();
+    HashMap<Integer, Integer> answerOne = new HashMap<>();
+    answerOne.put(2, 1);
+    answerOne.put(3, 1);
+    trueAns.add(answerOne);
+    Assertions.assertIterableEquals(calculatedAns, trueAns);
+  }
+
+  @Test
+  void correctAnswer2() {
+    List<HashMap<Integer, Integer>> calculatedAns =
+        Task1.findSolutions(4, Arrays.asList("2", "1"));
+    List<HashMap<Integer, Integer>> trueAns = new ArrayList<>();
+
+    HashMap<Integer, Integer> answerOne = new HashMap<>();
+    answerOne.put(1, 0);
+    answerOne.put(2, 2);
+    trueAns.add(answerOne);
+    HashMap<Integer, Integer> answerTwo = new HashMap<>();
+    answerTwo.put(1, 2);
+    answerTwo.put(2, 1);
+    trueAns.add(answerTwo);
+    HashMap<Integer, Integer> answerThree = new HashMap<>();
+    answerThree.put(1, 4);
+    answerThree.put(2, 0);
+    trueAns.add(answerThree);
+
+    Assertions.assertIterableEquals(calculatedAns, trueAns);
+  }
+
+  @Test
+  void correctAnswer3() {
+    List<HashMap<Integer, Integer>> calculatedAns =
+        Task1.findSolutions(1000, List.of("1"));
+    List<HashMap<Integer, Integer>> trueAns = new ArrayList<>();
+    HashMap<Integer, Integer> answerOne = new HashMap<>();
+    answerOne.put(1, 1000);
+    trueAns.add(answerOne);
+    Assertions.assertIterableEquals(calculatedAns, trueAns);
+  }
+
+  @Test
+  void correctAnswer4() {
+    List<HashMap<Integer, Integer>> calculatedAns =
+        Task1.findSolutions(5, List.of("10", "6"));
+    List<HashMap<Integer, Integer>> trueAns = new ArrayList<>();
+    Assertions.assertIterableEquals(calculatedAns, trueAns);
+  }
 }
