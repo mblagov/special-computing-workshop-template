a875572285c65a4d0d6adc386f0d05ebf6c99541
KashevarovStepan <st087947@student.spbu.ru>
2022-11-03 10:23:14 +0300

    Task3

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,49 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 /**
  * Задание 3.
  */
 public class Task3 {
 
-  public static void main(String[] args) {
+  public static final Logger logger = Logger.getLogger(Task3.class.getName());
 
+  /**
+   * Перебор дерева директорий, содержащихся в current.
+   *
+   * @param current - текущая директория/файл.
+   * @param depth   - текущая "глубина" относительно "коря".
+   * @return - дерево директорий в виде строки.
+   */
+  public static StringBuilder fileTree(File current, String depth) {
+    StringBuilder path = new StringBuilder(depth + current.getName() + "\n");
+    if (current.isDirectory()) {
+      for (File item : current.listFiles()) {
+        path.append(fileTree(item, depth + "-"));
+      }
+    } else {
+      return new StringBuilder(depth + current.getName() + "\n");
+    }
+    return path;
+  }
+
+  /**
+   * Записывает дерево из дериктории args[0] в файл args[1].
+   *
+   * @param args - args[0], args[1].
+   */
+  public static void main(String[] args) {
+    try (FileWriter writer = new FileWriter(args[1], false)) {
+      File folder = new File(args[0]);
+      String fileTree = String.valueOf(fileTree(folder, ""));
+      writer.write(fileTree);
+      writer.flush();
+    } catch (IOException exception) {
+      logger.info(exception.getMessage());
+    }
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,74 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
 
+  static class LogHandler extends Handler {
+
+    Level lastLevel = Level.FINEST;
+
+    public Level checkLevel() {
+      return lastLevel;
+    }
+
+    public void publish(LogRecord record) {
+      lastLevel = record.getLevel();
+    }
+
+    public void close() {
+    }
+
+    public void flush() {
+    }
+  }
+
+  @Test
+  void testMain() {
+    String expected = "test\n"
+        + "-testFile1.txt\n"
+        + "-testFolder1\n"
+        + "--testFolder1File1.txt\n"
+        + "-testFolder2\n"
+        + "--testFolder2File1.txt\n"
+        + "--testFolder2Folder1\n"
+        + "--testFolder2Folder2\n"
+        + "---testFolder2Folder2File1.txt\n"
+        + "---testFolder2Folder2File2.txt\n";
+    String[] args = new String[]{"C:\\Test\\test", "C:\\Test\\test.txt"};
+    Task3.main(args);
+    StringBuilder actual = new StringBuilder();
+    try (FileReader reader = new FileReader("C:\\Test\\test.txt")) {
+      int c;
+      while ((c = reader.read()) != -1) {
+        actual.append((char) c);
+      }
+    } catch (IOException exception) {
+      System.out.println(exception.getMessage());
+    }
+    Assertions.assertEquals(expected, actual.toString());
+  }
+
+  @Test
+  void testError() {
+    LogHandler handler = new LogHandler();
+    handler.setLevel(Level.ALL);
+    Task3.logger.setUseParentHandlers(false);
+    Task3.logger.addHandler(handler);
+    Task3.logger.setLevel(Level.ALL);
+    String[] args = new String[]{"C:\\Test\\test", "C:\\Test\\test"};
+    Task3.main(args);
+    Assertions.assertEquals(Level.INFO, handler.checkLevel());
+  }
 }
+
