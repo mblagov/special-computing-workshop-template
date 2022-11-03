b64ce3844c338df292161d791b8f9998ae3698d9
astatochek <71458498+astatochek@users.noreply.github.com>
2022-11-03 10:35:07 +0300

    add Tests

--- src/main/java/ru/spbu/apcyb/svp/tasks/task3/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task3/Task3.java
@@ -1,6 +1,7 @@
-package ru.spbu.apcyb.svp.tasks;
+package ru.spbu.apcyb.svp.tasks.task3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
@@ -13,6 +14,9 @@ public class Task3 {
   private static void getFileStructure(String dirPath, int level, FileWriter out)
       throws IOException {
     File dir = new File(dirPath);
+    if (!dir.exists()) {
+      throw new IOException("No such File or Directory: " + dir);
+    }
     String[] indentation = new String[level];
     Arrays.fill(indentation, "    ");
     out.write(
@@ -32,6 +36,10 @@ public class Task3 {
   public static void main(String[] args) throws IOException {
     String dirPath = args[0];
     String outPath = args[1];
+    File outCheck = new File(outPath);
+    if (!outCheck.exists()) {
+      throw new FileNotFoundException("Incorrect Path: " + outPath);
+    }
     FileWriter out = new FileWriter(outPath);
     getFileStructure(dirPath, 0, out);
     out.close();
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,65 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.spbu.apcyb.svp.tasks.task3.*;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
 
+  @Test
+  void worksCorrectly() throws IOException{
+    String dirPath = "src\\test";
+    String outPath = "src\\main\\java\\ru\\spbu\\apcyb\\svp\\tasks\\task3\\Task3.out";
+    Task3.main(new String[]{dirPath, outPath});
+    String[] EXPECTED = {
+        "test:",
+        "    java:",
+        "        ru:",
+        "            spbu:",
+        "                apcyb:",
+        "                    svp:",
+        "                        tasks:",
+        "                            ArrayUtilsTest.java",
+        "                            Task1Test.java",
+        "                            Task2Test.java",
+        "                            Task3Test.java",
+        "                            Task4Test.java",
+        "                            Task5Test.java",
+        "    resources:",
+        "        .stub"
+    };
+
+    Scanner scanner = new Scanner(new File(outPath));
+    scanner.useDelimiter("\n");
+
+    Assertions.assertTrue(scanner.hasNext());
+    for (String line : EXPECTED) {
+      Assertions.assertEquals(line, scanner.next());
+    }
+  }
+
+  @Test
+  void correctException1() {
+    String dirPath = "src\\tests";
+    String outPath = "src\\main\\java\\ru\\spbu\\apcyb\\svp\\tasks\\task3\\Task3.out";
+    Assertions.assertThrowsExactly(IOException.class, () -> {
+      Task3.main(new String[]{dirPath, outPath});
+    });
+  }
+
+  @Test
+  void correctException2() {
+    String dirPath = "src\\test";
+    String outPath = "src\\main\\java\\ru\\spbu\\apcyb\\svp\\tasks\\task\\Task3.out";
+    Assertions.assertThrowsExactly(FileNotFoundException.class, () -> {
+      Task3.main(new String[]{dirPath, outPath});
+    });
+  }
 }
