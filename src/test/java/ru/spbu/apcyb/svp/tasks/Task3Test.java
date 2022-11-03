ccade46bdf08702398597dea01cffe2917d8e24a
user <st085727@student.spbu.ru>
2022-11-03 09:32:28 +0300

    Task 3

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,7 @@
 package ru.spbu.apcyb.svp.tasks;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,16 +13,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class Task3Test {
 
     @Test
-    void test2() throws IOException {
-        FileWriter resFile = new FileWriter("RESULT.txt", false);
+    void testFileNotFound() throws IOException {
+        FileWriter resFile = new FileWriter("resTest1.txt", false);
         FileNotFoundException thrown =
                 assertThrows(FileNotFoundException.class, () -> Task3.getFileTree( resFile, "123"));
         assertEquals("java.io.FileNotFoundException", thrown.toString());
     }
 
     @Test
-    void test1() throws IOException {
-        FileWriter resFile = new FileWriter("RESULT.txt", false);
+    void testTrue() throws IOException {
+        FileWriter resFile = new FileWriter("resTest2.txt", false);
         boolean thrown = true;
         try {
             Task3.getFileTree( resFile, "..//");
@@ -31,6 +30,46 @@ class Task3Test {
             thrown = false;
         }
         assertTrue(thrown);
+    }
+
+    @Test
+    void testFirstLine() throws IOException {
+        FileWriter resFile = new FileWriter("resTest3.txt", false);
+        try {
+            Task3.getFileTree( resFile, "..//");
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
+        Path filePath = Path.of("resTest4.txt");
+        try (BufferedReader readBuffer = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String sCurrentLine = readBuffer.readLine();
+            assertEquals("..\\special-computing-workshop-template", sCurrentLine);
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
+    }
+
+
+    @Test
+    void testLastLine() throws IOException {
+        FileWriter resFile = new FileWriter("resTest4.txt", false);
+        try {
+            Task3.getFileTree( resFile, "..//");
+        } catch (IOException e) {
+
+            throw new FileNotFoundException();
+        }
+        Path filePath = Path.of("resTest4.txt");
+        try (BufferedReader readBuffer = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String sCurrentLine = readBuffer.readLine();
+            String sNextLine = sCurrentLine;
+            while ((sCurrentLine = readBuffer.readLine()) != null) {
+                sNextLine = sCurrentLine;
+            }
+            assertEquals("C:\\Users\\user\\IdeaProjects\\spec", sNextLine);
+        } catch (IOException e) {
+            throw new FileNotFoundException();
+        }
     }
 
 }
