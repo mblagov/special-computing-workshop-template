8213983a3576d01db92be6abe818449e2b215ab7
prostotema1 <artemkorolev-2002@yandex.ru>
2022-11-09 20:58:08 +0300

    Task 1 and Tests for Task 1

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task1.java
@@ -1,11 +1,100 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+
 /**
- * Задание 1.
+ * Задание 1. Банкомат и размены.
  */
+
 public class Task1 {
 
-  public static void main(String[] args) {
+    /**
+     * Метод для получения числа комбинаций купюр данной суммы.
+     *
+     * @param sum - переменная для передачи суммы
+     * @param prevNominal - переменная для хранения предыдущего номинала, участвующего в размене.
+     * @param combination - строка для хранения и вывода комбинации
+     * @param nomArr - массив номиналов
+     * @return число комбинаций
+     */
+    public static int getCombinations(int sum, int prevNominal, String combination, Integer[] nomArr) {
+        Logger logger = Logger.getLogger(Task1.class.getName());
+        int count = 0;
+        if (sum == 0) {
+            count++;
+            logger.info(combination);
+        }
+        for (int j : nomArr) {
+            if ((prevNominal >= j) && (sum >= j)) {
+                count += getCombinations(sum - j, j, combination + " " + j + " ", nomArr);
+            }
+        }
+        return count;
+    }
 
-  }
+    /**
+     * Метод для парсинга суммы.
+     *
+     * @param str - строка с введенной суммой
+     * @return сумма
+     */
+
+    public  static int parseSum(String str) {
+        int sum;
+        try {
+            sum = Integer.parseInt(str);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ошибка! Сумма - нецелое число.");
+        }
+        if (sum > 0) {
+            return sum;
+        } else {
+            throw new ArithmeticException("Ошибка! Сумма <= 0");
+        }
+    }
+
+    /**
+     * Метод для парсинга номиналов.
+     *
+     * @param str - строка с номиналами из ввода в консоли
+     * @return отсортированный массив номиналов
+     */
+
+    public  static Integer[] parseNominal(String str) {
+        String[] strMas = str.split(" ");
+        Integer[] nomArr = new Integer[strMas.length];
+        for (int i = 0; i < nomArr.length; i++) {
+            try {
+                nomArr[i] = Integer.parseInt(strMas[i]);
+                if (nomArr[i] <= 0) {
+                    throw new ArithmeticException("Ошибка! Значение номинала <= 0!");
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Ошибка! Значение номинала не целое число!");
+            }
+        }
+        Arrays.sort(nomArr, Collections.reverseOrder());
+        return (nomArr);
+    }
+
+    /**
+     * Main.
+     * Программа принимает от пользователя две строки из консоли
+     * str1 - сумма
+     * str2 - номиналы через пробел
+     * выводит комбинации и их число.
+     */
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Task1.class.getName());
+        Scanner scanner = new Scanner(System.in);
+        String sum = scanner.nextLine();
+        String nominal = scanner.nextLine();
+        String str = String.valueOf(getCombinations(parseSum(sum), parseNominal(nominal)[0], " ", parseNominal(nominal)));
+        logger.info(str);
+    }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task1Test.java
@@ -1,8 +1,118 @@
 package ru.spbu.apcyb.svp.tasks;
 
-/**
- * Тесты для задания 1.
- */
-public class Task1Test {
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+class Task1Test {
+    @Test
+    void combinations1() {
+        Integer[] arr = {200, 100, 50};
+        int actual = Task1.getCombinations(620, 200, "", arr);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinations2() {
+        Integer[] arr = {10, 6};
+        int actual = Task1.getCombinations(5, 10, "", arr);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinations3() {
+        Integer[] arr = {3, 2};
+        int actual = Task1.getCombinations(5, 3, "", arr);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinations4() {
+        Integer[] arr = {2, 1};
+        int actual = Task1.getCombinations(4, 2, "", arr);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinations5() {
+        Integer[] arr = {1, 2};
+        int actual = Task1.getCombinations(4, 2, "", arr);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinations7() {
+        Integer[] arr = {1, 2};
+        int actual = Task1.getCombinations(-1, 2, "", arr);
+        int expected = 0;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void combinations8() {
+        Integer[] arr = {};
+        int actual = Task1.getCombinations(200, 0, "", arr);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-100 50 20", "a", ""})
+    void checkNom(String arg) {
+        boolean thrown = false;
+        try {
+            Task1.parseNominal(arg);
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100 50 20", "100 20 50", "3 2", "1"})
+    void checkNom2(String arg) {
+        boolean thrown = true;
+        try {
+            Task1.parseNominal(arg);
+        } catch (Exception e) {
+            thrown = false;
+        }
+        assertTrue(thrown);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-100", "a", " "})
+    void checkSum(String arg) {
+        boolean thrown = false;
+        try {
+            Task1.parseSum(arg);
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "1", "1000"})
+    void checkSum2(String arg) {
+        boolean thrown = true;
+        try {
+            Task1.parseSum(arg);
+        } catch (Exception e) {
+            thrown = false;
+        }
+        assertTrue(thrown);
+    }
 
 }
