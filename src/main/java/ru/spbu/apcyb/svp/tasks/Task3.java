b0d9531c5c5da62276d4b50d4ae24aa97a307d8f
Zhgilev Ilya <st086873@student.spbu.ru>
2022-11-17 10:12:56 +0300

    Task3 added

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,49 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 /**
  * Задание 3.
  */
 public class Task3 {
 
-  public static void main(String[] args) {
+  protected void printFiles(File curRoot, FileWriter fileWriter) throws IOException {
+    for (File curFile : curRoot.listFiles()) {
+      if (curFile.isDirectory()) {
+        printFiles(curFile, fileWriter);
+      } else {
+        fileWriter.write(curFile.getPath().concat("\n"));
+      }
+    }
+  }
+
+  private void mainLogic(String root, String output) throws IOException {
+    File rootDir = new File(root);
+    if (!rootDir.exists() || !rootDir.isDirectory()) {
+      throw new IOException("error with dir");
+    }
+    File outputFile = new File(output);
+    try (FileWriter fileWriter = new FileWriter(outputFile)) {
+      this.printFiles(rootDir, fileWriter);
+    } catch (IOException e) {
+      throw new IOException("cant write into file");
+    }
+  }
+
+  /**
+   * Программа выводит содержимое дериктории в отдельный файл.
+   *
+   * @param args - путь до директории, содержимое которой необходимо вывести и путь до файла, в
+   *             который осуществляется вывод.
+   * @throws IOException ошибка в задании пути до дериктории.
+   */
+
+  public static void main(String[] args) throws IOException {
+    Task3 task3 = new Task3();
+    task3.mainLogic(args[0], args[1]);
 
   }
 }
