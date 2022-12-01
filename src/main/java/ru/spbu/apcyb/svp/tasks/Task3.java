305fdc6d867aa5e547a0aec7f1160f329add382a
Volodya-GoreProger <st087826@student.spbu.ru>
2022-12-01 12:06:18 +0300

    Сдаю Task3

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,77 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Задание 3.
  */
 public class Task3 {
+  
+  /**
+   * Вывод списков файлов и папок в заданном директории.
+   *
+   * @param dir (directory) директория
+   * @param file файл для вывода
+   * @param s вспомогательная переменная для красивого вывода с пробелами
+   * @throws FileNotFoundException если директория не существует или файл является директорией
+   */
+  static void fileFinder(String dir, File file, int s) throws IOException {
+    
+    //Проверка на директорию
+    if (file.isDirectory()) {
+      throw new FileNotFoundException("Записывающий файл является директорией!");
+    }
+  
+    try (FileWriter fr = new FileWriter(file, false)) {
+    
+      File root = new File(dir);
+      File[] list = root.listFiles();
+    
+      //Проверка на существование
+      if (list == null) {
+        throw new FileNotFoundException("Директория не существует");
+      }
+    
+      for (File f : list) {
+        
+        if (f.isDirectory()) {
+          
+          for (int i = 0; i < s; i++) {
+            fr.append(' ');
+          }
+          
+          fr.write("Директория: " + f.getPath());
+          fr.append('\n');
+  
+          fileFinder(f.getAbsolutePath(), file, s + 1);
+          
+        } else {
+          
+          for (int i = 0; i < s; i++) {
+            fr.append(' ');
+          }
+          
+          fr.write("Файл: " + f.getAbsoluteFile());
+          fr.append('\n');
+        }
+      }
+    }
 
-  public static void main(String[] args) {
-
+  }
+  
+  /**
+   * Самый обычный мейн.
+   *
+   * @param args самый обычный args
+   * @throws IOException самый обычный exception
+   */
+  public static void main(String[] args) throws IOException {
+    
+    File file = new File("answer.txt");
+  
+    fileFinder("..//", file, 0);
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,59 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
-
+class Task3Test {
+  
+  @Test
+  void fileFinderTest1() {
+    
+    String dir1 = "IAmNotExist";
+    File file1 = new File("answer.txt");
+    
+    FileNotFoundException thrown = assertThrows(FileNotFoundException.class, () -> Task3.fileFinder(dir1, file1, 0));
+    
+    assertEquals("java.io.FileNotFoundException: Директория не существует", thrown.toString());
+  
+    String dir2  = "..//";
+    //Здесь file вляется директорией
+    File file2 = new File("ans.txt");
+  
+    thrown = assertThrows(FileNotFoundException.class, () -> Task3.fileFinder(dir2, file2, 0));
+  
+    assertEquals("java.io.FileNotFoundException: Записывающий файл является директорией!", thrown.toString());
+  
+    String dir3  = "..//";
+    File file3 = new File("..//");
+  
+    thrown = assertThrows(FileNotFoundException.class, () -> Task3.fileFinder(dir3, file3, 0));
+  
+    assertEquals("java.io.FileNotFoundException: Записывающий файл является директорией!", thrown.toString());
+  }
+  
+  @Test
+  void fileFinderTest2() {
+    
+    String dir = "..//";
+    File file = new File("answer.txt");
+    boolean isWorking = true;
+    
+    try {
+      Task3.fileFinder(dir, file, 0);
+    } catch (IOException thrown) {
+      isWorking = false;
+    }
+    
+    assertTrue(isWorking);
+  }
+  
+  
 }
