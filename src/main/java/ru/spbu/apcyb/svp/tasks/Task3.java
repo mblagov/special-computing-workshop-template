848df77f9e82de2915c864c49c188448de3d9c47
Volodya-GoreProger <st087826@student.spbu.ru>
2022-12-01 11:03:31 +0300

    Сдаю Task3

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,63 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 /**
  * Задание 3.
  */
 public class Task3 {
-
+  
+  /**
+   * Вывод списков файлов и папок в заданном директории.
+   *
+   * @param dir (directory) директория
+   * @param fw файл для вывода
+   * @param s вспомогательная переменная для красивого вывода с пробелами
+   * @throws FileNotFoundException если директория не существует
+   */
+  static void walk(String dir, FileWriter fw, int s) throws FileNotFoundException {
+    
+    Logger logger = Logger.getLogger(Task3.class.getName());
+    
+    File root = new File(dir);
+    File[] list = root.listFiles();
+    
+    if (list == null) {
+      throw new FileNotFoundException();
+    }
+    
+    try {
+      for (File f : list) {
+        
+        if (f.isDirectory()) {
+          for (int i = 0; i < s; i++) {
+            fw.append(' ');
+          }
+          
+          fw.write("Directory: " + f.getPath());
+          fw.append('\n');
+          
+          walk(f.getAbsolutePath(), fw, s + 1);
+        } else {
+          
+          for (int i = 0; i < s; i++) {
+            fw.append(' ');
+          }
+          
+          fw.write("File: " + f.getAbsoluteFile());
+          fw.append('\n');
+        }
+      }
+    } catch (IOException ex) {
+      logger.info(ex.getMessage());
+    }
+  }
+  
   public static void main(String[] args) {
-
+    //useless :(
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,42 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
-
+class Task3Test {
+  
+  @Test
+  void testInput1() throws IOException {
+  
+    String path = "";
+    String file = "answer.txt";
+    
+    try (FileWriter writer = new FileWriter(file, false)) {
+      Task3.walk(path, writer, 0);
+    } catch (FileNotFoundException thrown) {
+      assertEquals("java.io.FileNotFoundException", thrown.toString());
+    }
+    
+  }
+  
+  @Test
+  void testInput2() throws IOException {
+  
+    String path = "FFF";
+    String file = "answer.txt";
+    
+    try (FileWriter writer = new FileWriter(file, false)) {
+      Task3.walk(path, writer, 0);
+    } catch (FileNotFoundException thrown) {
+      assertEquals("java.io.FileNotFoundException", thrown.toString());
+    }
+    
+  }
 }
