0d209340e2b094457241f6dfccf4b0d8a549e612
maija <mayyaandreevna@yandex.ru>
2022-12-07 14:49:45 +0300

    сделана 3я задача с покрытием 71

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,46 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Задание 3.
  */
 public class Task3 {
 
-  public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(Task3.class);
 
-  }
+    public static void walkFileTree(String path, FileWriter fileWriter) throws IOException {
+        File f = new File(path);
+        if (!f.exists()) {
+            throw new FileNotFoundException();
+        }
+        try {
+            fileWriter.write(f.getPath() + "\n");
+            if (f.listFiles() != null) {
+                for (File f1 : f.listFiles()) {
+                    walkFileTree(f1.getPath(), fileWriter);
+                }
+            }
+
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+        }
+    }
+
+    public static boolean walkFileTree(String path, String fileWriterName) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileWriterName, false);
+        walkFileTree(path, fileWriter);
+        fileWriter.flush();
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        walkFileTree("[[", "task3.txt");
+    }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,36 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static ru.spbu.apcyb.svp.tasks.Task3.walkFileTree;
+
 /**
  * Тесты для задания 3.
  */
 public class Task3Test {
+
+    @Test
+    void isAbsoluteRef() throws IOException {
+        boolean result = walkFileTree("/Users/mayya/IdeaProjects/special-computing-workshop-template/target/classes", "task3.txt");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void isRelativeRef() throws IOException {
+        boolean result = walkFileTree("..//", "task3.txt");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void isIncorrectInput() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+            walkFileTree("[[", "task3.txt");
+        });
+    }
+
 
 }
