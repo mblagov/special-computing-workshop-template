1b0a1441f0bd92fb6d9033e2edd922b2afb7a611
maija <mayyaandreevna@yandex.ru>
2022-12-08 16:44:07 +0300

    аргументы сделала через main, добавила исключений на случай неправильного ввода, покрытие 75, сообщения об ошибках сделала подробными

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -15,11 +15,13 @@ public class Task3 {
 
     private static final Logger logger = LogManager.getLogger(Task3.class);
 
-    public static void walkFileTree(String path, FileWriter fileWriter) throws IOException {
+    public static void walkFileTree(String path, FileWriter fileWriter) throws IOException, FileNotFoundException {
         File f = new File(path);
         if (!f.exists()) {
+            logger.fatal("Incorrect input: file (first arg) is not found");
             throw new FileNotFoundException();
         }
+
         try {
             fileWriter.write(f.getPath() + "\n");
             if (f.listFiles() != null) {
@@ -34,6 +36,12 @@ public class Task3 {
     }
 
     public static boolean walkFileTree(String path, String fileWriterName) throws IOException {
+        File f = new File(fileWriterName);
+        if (f.isDirectory()) {
+            logger.fatal("Incorrect input: FileWriter's name is taken by directory");
+            throw new RuntimeException();
+        }
+
         FileWriter fileWriter = new FileWriter(fileWriterName, false);
         walkFileTree(path, fileWriter);
         fileWriter.flush();
@@ -41,6 +49,13 @@ public class Task3 {
     }
 
     public static void main(String[] args) throws IOException {
-        walkFileTree("[[", "task3.txt");
+        if (args.length != 2) {
+            logger.fatal("Incorrect input: too many arguments");
+            throw new RuntimeException();
+        }
+
+        String path = args[0];
+        String fileWriterName = args[1];
+        walkFileTree(path, fileWriterName);
     }
 }
