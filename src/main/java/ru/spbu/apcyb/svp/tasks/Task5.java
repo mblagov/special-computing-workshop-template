6c52fe3c5728735c129f2a239ae91c6da53af0cf
user <st085727@student.spbu.ru>
2022-12-10 10:42:17 +0300

    Task 4 changes

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
@@ -1,11 +1,107 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Задание 5.
  */
 public class Task5 {
 
-  public static void main(String[] args) {
+    /**
+     * Выводит в существующий для каждого слова файл, с ним же в названии,
+     * это слово ровно столько раз, сколько оно встретилось в исходном тексте.
+     *
+     * @param MASS - массив, содержащий само слово и число его вхождений в текст.
+     *             MASS[0] - слово.
+     *             MASS[1] - число вхождений.
+     */
+    public static void wordsToExistingFile(String[] MASS) throws FileNotFoundException {
+        try (BufferedWriter wordFile = new BufferedWriter
+                (new OutputStreamWriter(new FileOutputStream((MASS[0] + ".txt"))))) {
+            for (int i = 0; i < Integer.parseInt(MASS[1]); i++) {
+                wordFile.write(MASS[0]);
+                wordFile.write("\n");
+            }
+        } catch (IOException e) {
+            throw new FileNotFoundException("file for word " + MASS[0] + "did not open");
+        }
+    }
 
-  }
+    /**
+     * Создает файл для каждого слова, с ним же в названии.
+     *
+     * @param MASS - массив, содержащий само слово и число его вхождений в текст.
+     * Принимает весь массив, чтобы отправить его в wordsToExistingFile.
+     *             MASS[0] - слово.
+     */
+    public static void wordsToNonExistingFile(String[] MASS) throws FileNotFoundException {
+        try (FileWriter wordFile = new FileWriter(MASS[0] + ".txt", true)) {
+            wordsToExistingFile(MASS);
+        } catch (IOException ex) {
+            throw new FileNotFoundException("new file for new word " + MASS[0] + "did not open");
+        }
+    }
+
+    /**
+     * Считает число вхождений каждого слова в текст,
+     * выводит данные в файл с результатом, и в зависимости от того,
+     * встречалось уже слово или нет, вызывает методы wordsToNonExistingFile и wordsToExistingFile.
+     *
+     * @param dataFileName - название файла с исходным текстом.
+     * @param countsFileName - название файла для вывода данных.
+     */
+    public static void textFileSort(String dataFileName, String countsFileName) throws FileNotFoundException {
+        try (Stream<String> stream = Files.lines(Paths.get(dataFileName));
+             BufferedWriter toCountsFile = new BufferedWriter
+                     (new OutputStreamWriter(new FileOutputStream((countsFileName))))) {
+            stream.flatMap(line -> Arrays.stream(line.trim().split(" ")))
+                    .map(word -> word.replaceAll("\\P{L}", " ").trim())
+                    .map(String::toLowerCase)
+                    .filter(word -> word.length() > 0)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .forEach(word -> {
+                        try {
+                            toCountsFile.write(word + "\n");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        String[] MASS = String.valueOf(word).split("=");
+                        if (Files.exists(Path.of(MASS[0] + ".txt"))) {
+                            CompletableFuture.runAsync(() -> {
+                                try {
+                                    wordsToExistingFile(MASS);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                        } else {
+                            CompletableFuture.runAsync(() -> {
+                                try {
+                                    wordsToNonExistingFile(MASS);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                        }
+                    });
+        } catch (IOException e) {
+            throw new FileNotFoundException("problems with " + dataFileName + " or " + countsFileName + " file");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        textFileSort("aaaaaaaaaa.txt", "counts.txt");
+    }
+
+
 }
+
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
@@ -1,8 +1,112 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Тесты для задания 5.
  */
-public class Task5Test {
+class Task5Test {
 
+    /**
+     * Проверяет, есть ли файл с таким названием.
+     * Если нет, то запускает метод.
+     * Проверяет, что файл появился.
+     */
+    @Test
+    void wordsToNonExistingFile() throws IOException {
+        boolean fileDidNotExist = false;
+        boolean fileExists = false;
+        boolean check = false;
+        String[] word = {"toyotaravfour", "1"};
+        Path path =
+                Path.of(word[0] + ".txt");
+        if (!Files.exists(path)) {
+            Task5.wordsToNonExistingFile(word);
+            fileDidNotExist = true;
+        }
+        if (Files.exists(path)) {
+            fileExists = true;
+        }
+        if ((fileExists) && (fileDidNotExist)) {
+            check = true;
+        }
+        assertTrue(check);
+        Files.delete(path);
+    }
+
+    /**
+     * Проверяет, вывелось ли слово в нужный файл.
+     */
+    @Test
+    void wordsToExistingFile() throws FileNotFoundException {
+        String[] testWord = {"underdog", "1"};
+        Task5.wordsToExistingFile(testWord);
+        Path filePath = Path.of("underdog.txt");
+        try (BufferedReader fromUnderdog = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String currentLine = fromUnderdog.readLine();
+            assertEquals("underdog", currentLine);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Проверяет, создались ли файлы под слова.
+     */
+    @Test
+    void textFileSort() throws IOException {
+        int count = 0;
+        Task5.textFileSort("task5ReferenceTestFile.txt", "countsTestFile.txt");
+        String[] words = {"underdog", "supercat", "toyotaravfour", "huperman"};
+        for (String current : words) {
+            Path path = Path.of(current + ".txt");
+            if (Files.exists(path)) {
+                count++;
+                Files.delete(path);
+            }
+        }
+        assertEquals(4, count);
+    }
+
+    /**
+     * Проверяет, правильные ли данные оказались в файле результата.
+     */
+    @Test
+    void textFileSort2() {
+        int numberOfRightLines = 0;
+        Path filePath = Path.of("countsTestFile");
+        String[] words = {"underdog=1", "supercat=1", "toyotaravfour=1", "huperman=1"};
+        try (BufferedReader fromCountsTestFile = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String currentLine;
+            for (int i = 0; i < 4; i++) {
+                currentLine = fromCountsTestFile.readLine();
+                if (currentLine.equals(words[i])) {
+                    numberOfRightLines++;
+                }
+            }
+            assertEquals(4, numberOfRightLines);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Проверяет ошибку, при чтении исходного текста из несуществующего файла.
+     * Или при выводе результата в несуществующий файл
+     */
+    @Test
+    void textFileSortEx() {
+        FileNotFoundException thrown =
+                assertThrows(FileNotFoundException.class, () -> Task5.textFileSort("NonExistingFile.txt", "countsTestFile.txt"));
+        assertEquals("problems with NonExistingFile.txt or countsTestFile.txt file", thrown.getMessage());
+    }
 }
