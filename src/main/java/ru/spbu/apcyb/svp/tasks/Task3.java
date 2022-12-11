61ebdc90df5f09188784724196f2e37d76616010
artemiytoropchin <artemiy.toropchin@gmail.com>
2022-12-11 18:57:38 +0300

    3 task

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,46 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.NotDirectoryException;
+import java.util.Scanner;
+
 /**
  * Задание 3.
  */
 public class Task3 {
+  public static void main(String[] args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    String path = scanner.nextLine();
+    String file2Save = scanner.nextLine();
+    scanner.close();
 
-  public static void main(String[] args) {
-
+    FileWriter writer = new FileWriter(file2Save);
+    File currentDir = new File(path).getAbsoluteFile();
+    displayDirectoryContents(currentDir, writer);
+    writer.close();
   }
+
+  public static void displayDirectoryContents(File dir, FileWriter writer)
+      throws IOException {
+    if (!dir.exists()) {
+      throw new FileNotFoundException("directory doesn't exists");
+    } else if (!dir.isDirectory()) {
+      throw new NotDirectoryException("it's not a directory");
+    }
+
+    File[] files = dir.listFiles();
+    assert files != null;
+    for (File file : files) {
+      if (file.isDirectory()) {
+        writer.write("directory:" + file.getCanonicalPath() + "\n");
+        displayDirectoryContents(file, writer);
+      } else {
+        writer.write("     file:" + file.getCanonicalPath() + "\n");
+      }
+    }
+  }
+
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,60 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
+  @Test
+  void dirNotExists() {
+    boolean thrown = false;
+
+    try {
+      FileWriter file = new FileWriter("testfile");
+      File dir = new File("skgsvlnsxckmvnxclv").getAbsoluteFile();
+      Task3.displayDirectoryContents(dir, file);
+    } catch (IOException e) {
+      thrown = true;
+    }
+
+    assertTrue(thrown);
+  }
+
+  @Test
+  void fileInInexistingFolder() {
+    boolean thrown = false;
+
+    try {
+      FileWriter file = new FileWriter("ggsgsgsgsdgssdgs");
+      File dir = new File("/afaf/afaf").getAbsoluteFile();
+      Task3.displayDirectoryContents(dir, file);
+    } catch (IOException e) {
+      thrown = true;
+    }
+
+    assertTrue(thrown);
+  }
+
+  @Test
+  void normalRun() {
+    boolean thrown = false;
+
+    try {
+      FileWriter file = new FileWriter("testfile");
+      File dir = new File(".").getAbsoluteFile();
+      Task3.displayDirectoryContents(dir, file);
+    } catch (IOException e) {
+      thrown = true;
+    }
+
+    assertFalse(thrown);
+  }
 
 }
