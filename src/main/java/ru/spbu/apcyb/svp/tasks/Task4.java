e1060f97c38406890243de078b7a449ceb62939b
Volodya-GoreProger <st087826@student.spbu.ru>
2022-12-15 22:29:53 +0300

    Сдаю Task4

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
@@ -1,11 +1,159 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Задание 4.
  */
 public class Task4 {
-
-  public static void main(String[] args) {
-
+  
+  private static final Logger logger = LogManager.getLogger(Task4.class);
+  
+  /** Метод вычисления tg.
+   *
+   * @param numbOfLine - номер строки
+   * @return значение tg
+   * @throws FileNotFoundException - если файл был не найден
+   */
+  public static Double tanFromFileLine(int numbOfLine) throws FileNotFoundException {
+    
+    try (BufferedReader br = new BufferedReader(new FileReader("x.txt"))) {
+      String line = "";
+      
+      for (int i = 0; i < numbOfLine; i++) {
+        line = br.readLine();
+      }
+      
+      return Math.tan(Double.parseDouble(line));
+      
+    } catch (IOException ex) {
+      throw new FileNotFoundException("Файл.txt не был найден.");
+    }
+  }
+  
+  /** Метод вычисления tg в многопоточном режиме.
+   *
+   * @param fw - fileWriter для "MultiThread.txt"
+   * @param numbOfLines - количество строк для вычисления
+   * @param numbOfThreads - количество потоков
+   * @return время работы
+   * @throws ExecutionException если не получается получить значение из future
+   * @throws InterruptedException если было прервано получение значения из future
+   * @throws IOException если не удалось открыть файл
+   */
+  public static long multiThread(FileWriter fw, int numbOfLines, int numbOfThreads)
+      throws ExecutionException, InterruptedException, IOException {
+  
+    final long start = System.nanoTime();
+    ThreadPool threadPool = new ThreadPool(numbOfThreads);
+    List<Future<Double>> futures = new ArrayList<>();
+    
+    for (int i = 0; i < numbOfLines / numbOfThreads; i++) {
+      for (int j = 1; j <= numbOfThreads; j++) {
+        
+        final int k = j + i * numbOfThreads;
+        
+        futures.add(
+            CompletableFuture.supplyAsync(
+              () -> {
+                try {
+                  return tanFromFileLine(k);
+                } catch (Exception e) {
+                  throw new RuntimeException(e);
+                }
+              },
+              
+            threadPool
+          ));
+      }
+    }
+    
+    for (Future<Double> future : futures) {
+      fw.write(String.valueOf(future.get()));
+      fw.append("\n");
+    }
+    
+    threadPool.shutdown();
+    
+    return (System.nanoTime() - start);
+  }
+  
+  /** Метод вычисления tg в однопоточном режиме.
+   *
+   * @param fw - fileWriter для "SingleThread.txt"
+   * @param numbOfLines - количество строк для вычисления
+   * @return время работы
+   * @throws FileNotFoundException если файл не был найден
+   */
+  public static long singleThread(FileWriter fw, int numbOfLines) throws FileNotFoundException {
+    
+    long start = System.nanoTime();
+    
+    try (BufferedReader br = new BufferedReader(new FileReader("x.txt"))) {
+      
+      String line;
+      long helper = 0;
+      
+      while ((line = br.readLine()) != null && helper < numbOfLines) {
+        
+        fw.write(String.valueOf(Math.tan(Double.parseDouble(line))));
+        fw.append("\n");
+        
+        helper++;
+      }
+    } catch (Exception e) {
+      throw new FileNotFoundException(e.toString());
+    }
+    
+    return System.nanoTime() - start;
+  }
+  
+  
+  /** Самый обычный main.
+   *
+   * @param args самый обычный args
+   * @throws IOException если FileWriter не создался
+   * @throws ExecutionException если не получается получить значение из future
+   * @throws InterruptedException если было прервано получение значения из future
+   */
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    
+    int numbOfLines = 1000;
+    int numbOfThreads = 10;
+    
+    long timeS = singleThread(new FileWriter("SingleThread.txt", false), numbOfLines);
+    long timeM = multiThread(new FileWriter("MultiThread.txt", false), numbOfLines, numbOfThreads);
+  
+    StringBuilder str = new StringBuilder();
+    str.append("При количестве строк = ")
+      .append(numbOfLines).append("\n")
+      .append("Количестве потоков = ")
+      .append(numbOfThreads).append("\n")
+      .append("Однопоточное время = ")
+      .append(timeS).append(" наносек\n")
+      .append("Многопоточное время = ")
+      .append(timeM).append(" наносек\n")
+        .append("\nВ данном случае лучше - ");
+    
+    if (timeS > timeM) {
+      str.append("Многопоточный режим;\n");
+    } else {
+      str.append("Однопоточный режим;\n");
+    }
+    
+    logger.log(Level.INFO, str);
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
@@ -1,8 +1,73 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 4.
  */
-public class Task4Test {
+class Task4Test {
+  
+  @Test
+  void testException1() {
+    
+    FileNotFoundException thrown = assertThrows(FileNotFoundException.class,
+      () -> Task4.singleThread(new FileWriter("error.txt", false), 100));
+    
+    assertEquals("Файл.txt не был найден.", thrown.getMessage());
+  }
+  
+  @Test
+  void testException2() throws IOException {
+    
+    try {
+      FileWriter fw = new FileWriter("error1.txt", false);
+    } catch (FileNotFoundException e) {
+      assertEquals("error1.txt (Is a directory)", e.getMessage());
+    }
+  }
+  
+  @Test
+  void testSingleThread() throws IOException {
+    
+    Task4.singleThread(new FileWriter("SingleTreads.txt", false), 1000);
+    BufferedReader xFile = new BufferedReader(new FileReader("x.txt"));
+    BufferedReader singleFile = new BufferedReader(new FileReader("SingleThread.txt"));
+    boolean result = true;
+    
+    for (int i = 0; i < 1000; i++) {
+      if (Math.tan(Double.parseDouble(xFile.readLine())) != Double.parseDouble(singleFile.readLine())) {
+        result = false;
+        break;
+      }
+    }
+    
+    assertTrue(result);
+  }
+  
+  @Test
+  void testMultiThread() throws IOException, ExecutionException, InterruptedException {
 
+    Task4.multiThread(new FileWriter("MultiRes.txt", false), 1000, 10);
+    BufferedReader xFile = new BufferedReader(new FileReader("x.txt"));
+    BufferedReader multiFile = new BufferedReader(new FileReader("MultiThread.txt"));
+    boolean result = true;
+    
+    for (int i = 0; i < 100; i++) {
+      if (Math.tan(Double.parseDouble(xFile.readLine())) != Double.parseDouble(multiFile.readLine())) {
+        result = false;
+        break;
+      }
+    }
+    assertTrue(result);
+  }
 }
