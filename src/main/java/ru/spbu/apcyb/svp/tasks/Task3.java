26c0092511c04e6b7ee048f7746e537eece0773f
Ekaterina Antropova <56113701+KateAntropova-4415@users.noreply.github.com>
2022-12-26 00:17:35 +0300

    Task 3

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,47 @@
 package ru.spbu.apcyb.svp.tasks;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * Задание 3.
  */
 public class Task3 {
 
-  public static void main(String[] args) {
+  static void fFinder(String direct, File file, int s) throws IOException {
 
+    try (FileWriter filewr = new FileWriter(file, false)) {
+      File root = new File(direct);
+      File[] list = root.listFiles();
+      if (list == null) {
+        throw new FileNotFoundException("Директория не существует");
+      }
+      for (File f : list) {
+        if (f.isDirectory()) {
+          for (int i = 0; i < s; i++) {
+            filewr.append(' ');
+          }
+          filewr.write("Директория: " + f.getPath());
+          filewr.append('\n');
+          fFinder(f.getAbsolutePath(), file, s + 1);
+        }
+        else {
+          for (int i = 0; i < s; i++) {
+            filewr.append(' ');
+          }
+          filewr.write("Файл: " + f.getAbsoluteFile());
+          filewr.append('\n');
+        }
+      }
+    }
+    if (file.isDirectory()) {
+      throw new FileNotFoundException("Записывающий файл - директория.");
+    }
+  }
+  public static void main(String[] args) throws IOException {
+    File file = new File("answ.txt");
+    fFinder("..//", file, 0);
   }
 }
