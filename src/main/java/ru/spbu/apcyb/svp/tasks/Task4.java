f9c3ef25bb1a98259ca3f9e95d26fb8c7e19083b
artemiytoropchin <artemiy.toropchin@gmail.com>
2022-12-25 19:57:10 +0300

    4 task

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
@@ -1,11 +1,60 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class CalculateTan implements Runnable {
+  String outFile;
+  float value;
+
+  public CalculateTan(float value, String outFile) {
+    this.value = value;
+    this.outFile = outFile;
+  }
+
+  @Override
+  public void run() {
+    try (FileWriter writer = new FileWriter(outFile, true)){
+      writer.write(String.valueOf(Math.tan(this.value)));
+      writer.write("\n");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
+
 /**
  * Задание 4.
  */
 public class Task4 {
+  public static void multiThreadTan(String inFile, String outFile, int numberOfLines, int numberOfThreads)
+      throws FileNotFoundException {
+    try (BufferedReader readBuffer = new BufferedReader(new FileReader(inFile))) {
+      new FileWriter(outFile, false).close(); // just to clear file
 
-  public static void main(String[] args) {
+      long startTime = System.nanoTime();
 
+      ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+      for (int i = 0; i < numberOfLines; i++) {
+        float val = Float.parseFloat(readBuffer.readLine());
+        executorService.execute(new CalculateTan(val, outFile));
+      }
+      executorService.shutdown();
+
+      long endTime = System.nanoTime();
+      long timeDelta = endTime - startTime;
+      System.out.println("MultiThread:   " + timeDelta);
+    } catch (IOException e) {
+      throw new FileNotFoundException("incorrect input file");
+    }
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    multiThreadTan("numbers", "testfile", 10000, 10);
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
@@ -1,8 +1,34 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 4.
  */
-public class Task4Test {
+class Task4Test {
+  @Test
+  void inputFileNotExists() {
+    assertThrows(
+        FileNotFoundException.class,
+        () -> Task4.multiThreadTan("afjafja1fanfa", "testfile", 1, 1)
+    );
+  }
+
+  @Test
+  void fileInInexistingFolder() {
+    assertThrows(
+        FileNotFoundException.class,
+        () -> Task4.multiThreadTan("numbers", "/a/b/c/d/e/f", 1, 1)
+    );
+  }
+
+  @Test
+  void normalRun() throws IOException {
+    Task4.multiThreadTan("numbers", "testfile", 10000, 10);
+  }
 
 }
