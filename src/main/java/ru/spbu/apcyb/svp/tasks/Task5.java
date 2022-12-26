b9f7a4b7a7dd7c81566f59a6ccdf608a8ba144eb
artemiytoropchin <artemiy.toropchin@gmail.com>
2022-12-26 22:33:19 +0300

    5 task

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
@@ -1,11 +1,61 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Задание 5.
  */
 public class Task5 {
 
-  public static void main(String[] args) {
+  public static void streamProccessWords(String textFile, String wordsCountFile) throws IOException {
+    try(Stream<String> stream = Files.lines(Paths.get(textFile));
+        FileWriter outFile = new FileWriter(wordsCountFile, false)) {
 
+        stream
+            .flatMap(line -> Arrays.stream(line.split(" ")))
+            .filter(word -> word.length() > 0)
+            .map(String::toLowerCase)
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .entrySet()
+            .forEach(pair -> {
+              CompletableFuture.runAsync(() -> {
+                try {
+                  writeWords2FIle(pair.getKey(), pair.getValue());
+                } catch (FileNotFoundException e) {
+                  throw new RuntimeException(e);
+                }
+              });
+              try {
+                outFile.write(pair + "\n");
+              } catch (IOException e) {
+                throw new RuntimeException("can't write word \"" + pair.getKey() + "\" and their count to file");
+              }
+            });
+
+    } catch (IOException e) {
+      throw new FileNotFoundException("some issues with \"" + wordsCountFile + "\" or \"" + textFile);
+    }
   }
+
+  public static void writeWords2FIle(String word, Long wordNumber) throws FileNotFoundException {
+    try (FileWriter outFile = new FileWriter(word, false)) {
+      for (int i = 0; i < wordNumber; i++) {
+        outFile.write(word + "\n");
+      }
+    } catch (IOException e) {
+      throw new FileNotFoundException("\"" + word + "\" didn't opened");
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    streamProccessWords("text", "counts");
+  }
+
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
@@ -1,8 +1,75 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Тесты для задания 5.
  */
-public class Task5Test {
+class Task5Test {
+
+  @Test
+  void normalRun() {
+    assertDoesNotThrow(
+        () -> Task5.streamProccessWords("text", "numbers")
+    );
+  }
+
+  @Test
+  void inFileInIncorrectInput() {
+    assertThrows(
+        FileNotFoundException.class,
+        () -> Task5.streamProccessWords("", "/a/b/c/d/e/f")
+    );
+  }
+
+  @Test
+  void outFileInInexistingFolder() {
+    assertThrows(
+        FileNotFoundException.class,
+        () -> Task5.streamProccessWords("text", "/a/b/c/d/e/f")
+    );
+  }
+
+  @Test
+  void correctWrite2File() throws FileNotFoundException {
+    Task5.writeWords2FIle("abcdefgh", (long)1);
+    try (BufferedReader readBuffer = new BufferedReader(new FileReader("abcdefgh"))) {
+      String currentLine = readBuffer.readLine();
+      assertEquals("abcdefgh", currentLine);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  void correctWordsProcess() throws IOException {
+    String[] words = {"a", "aa"};
+
+    for (String fileName : words) {
+      File file = new File(fileName);
+      if (file.exists()) {
+        file.delete();
+      }
+    }
+
+    Task5.streamProccessWords("text", "counts");
+    int count = 0;
+    for (String fileName : words) {
+      File file = new File(fileName);
+      if (file.exists()) {
+        count++;
+        file.delete();
+      }
+    }
+    assertEquals(2, count);
+  }
 
 }
