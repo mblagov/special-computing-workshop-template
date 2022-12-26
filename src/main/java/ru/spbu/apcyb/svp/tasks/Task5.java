b84fd4b0e9545d90f500d4b1e8d3181c423f9c98
KashevarovStepan <st087947@student.spbu.ru>
2022-12-26 17:35:26 +0300

    Task5

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
@@ -1,11 +1,123 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Задание 5.
  */
 public class Task5 {
 
-  public static void main(String[] args) {
+  public static final Logger logger = Logger.getLogger(Task5.class.getName());
+  public static final String PATH = "./src/main/resources/Task5/";
+  public static final String RESOURCES = "./src/main/resources/Task5/resources/";
 
+  /**
+   * Выводит в существующий для слова word файл(с названием word.txt) это слово ровно столько раз,
+   * сколько оно встретилось в исходном тексте.
+   *
+   * @param word    слово.
+   * @param counter число вхождений.
+   */
+  public static void writeWords(String word, Integer counter)
+      throws FileNotFoundException {
+    try (BufferedWriter writer = new BufferedWriter(
+        new OutputStreamWriter(new FileOutputStream(PATH + word + ".txt")))) {
+      for (int i = 0; i < counter; i++) {
+        writer.write(word + "\n");
+      }
+    } catch (IOException e) {
+      throw new FileNotFoundException(
+          "Не получилось открыть файл с названием \"" + word + ".txt\"");
+    }
+  }
+
+  /**
+   * Создает файл для слова word(с названием word.txt).
+   *
+   * @param word    слово.
+   * @param counter число вхождений.
+   */
+  public static void createFile(String word, Integer counter)
+      throws FileNotFoundException {
+    try (FileWriter wordFile = new FileWriter(PATH + word + ".txt", true)) {
+      writeWords(word, counter);
+    } catch (IOException e) {
+      throw new FileNotFoundException(
+          "Не получилось создать файл \"" + word + ".txt\"");
+    }
+  }
+
+  /**
+   * Считает число вхождений каждого слова в книгу bookFile, выводит данные в файл с результатом
+   * counterFile, и в зависимости от того, встречалось уже слово или нет, вызывает методы writeWords
+   * и createFile.
+   *
+   * @param bookFile    название файла с исходным текстом.
+   * @param counterFile название файла для вывода данных.
+   */
+  public static void processBook(String bookFile, String counterFile)
+      throws IOException {
+    try (Stream<String> stream = Files.lines(Paths.get(bookFile));
+        BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(new FileOutputStream(counterFile)))) {
+      stream.flatMap(line -> Arrays.stream(line.trim().split(" ")))
+          .map(word -> word.replaceAll("\\P{L}", " ").trim())
+          .map(String::toLowerCase)
+          .filter(word -> word.length() > 0)
+          .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+          .entrySet()
+          .forEach(word -> {
+            try {
+              writer.write(word + "\n");
+            } catch (IOException e) {
+              throw new RuntimeException("Ошибка с файлом counts.txt");
+            }
+            List<String> arr = List.of(word.toString().split("="));
+            String currentWord = arr.get(0);
+            Integer counter = Integer.parseInt(arr.get(1));
+            if (Files.exists(Path.of(currentWord + ".txt"))) {
+              CompletableFuture.runAsync(() -> {
+                try {
+                  writeWords(currentWord, counter);
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              });
+            } else {
+              CompletableFuture.runAsync(() -> {
+                try {
+                  createFile(currentWord, counter);
+                } catch (IOException e) {
+                  e.printStackTrace();
+                }
+              });
+            }
+          });
+    } catch (IOException e) {
+      throw new FileNotFoundException(
+          "Ошибка с \"" + bookFile + "\" или \"" + counterFile + "\"!");
+    }
+  }
+
+  /**
+   * Просто мейн.
+   */
+  public static void main(String[] args) throws IOException {
+    processBook(RESOURCES + "book.txt", RESOURCES + "counts.txt");
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
@@ -1,8 +1,103 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 5.
  */
-public class Task5Test {
+class Task5Test {
 
+  /**
+   * Проверка на создание нового файла.
+   */
+  @Test
+  void createFileTest() throws IOException {
+    boolean absent = false;
+    boolean exists = false;
+    boolean check = false;
+    Path path = Path.of(Task5.PATH + "nonExistent" + ".txt");
+    if (!Files.exists(path)) {
+      Task5.createFile("nonExistent", 1);
+      absent = true;
+    }
+    if (Files.exists(path)) {
+      exists = true;
+    }
+    if ((exists) && (absent)) {
+      check = true;
+    }
+    assertTrue(check);
+    Files.delete(path);
+  }
+
+  /**
+   * Проверка на запись слова в файл.
+   */
+  @Test
+  void writeWordsTest() throws FileNotFoundException {
+    String expected = "newWord";
+    Task5.writeWords(expected, 1);
+    try (BufferedReader reader = new BufferedReader(
+        new FileReader(Task5.PATH + expected + ".txt"))) {
+      String actual = reader.readLine();
+      assertEquals(expected, actual);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Проверка на создание отдельных файлов для каждого слова из книги.
+   */
+  @Test
+  void processBookCreationTest() throws IOException {
+    int actual = 0;
+    Task5.processBook(Task5.RESOURCES + "bookTest1.txt", Task5.RESOURCES + "countsTest1.txt");
+    List<String> words = List.of("five", "six", "seven");
+    for (String word : words) {
+      Path path = Path.of(Task5.PATH + word + ".txt");
+      if (Files.exists(path)) {
+        actual++;
+        Files.delete(path);
+      } else {
+        Task5.logger.info("Проблема со словом \"" + word + "\"");
+      }
+    }
+    assertEquals(words.size(), actual);
+  }
+
+  /**
+   * Проверка правильности данных в файле counts.txt.
+   */
+  @Test
+  void processBookOutputTest() throws IOException {
+    int actual = 0;
+    List<String> words = List.of("one=2", "two=1", "three=1");
+    Task5.processBook(Task5.RESOURCES + "bookTest2.txt", Task5.RESOURCES + "countsTest2.txt");
+    try (BufferedReader reader = new BufferedReader(
+        new FileReader(Task5.RESOURCES + "countsTest2.txt"))) {
+      String line;
+      for (int i = 0; i < words.size(); i++) {
+        line = reader.readLine();
+        if (words.contains(line)) {
+          actual++;
+        } else {
+          Task5.logger.info("Проблема со словом: " + line);
+        }
+      }
+      assertEquals(words.size(), actual);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
