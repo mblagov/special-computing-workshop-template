2f5ca74165c19edfdf869252504818b9d5a5cfa8
Artem Fedoseev <johndoe@example.com>
2022-12-27 04:54:08 +0300

    Task 4 with tests

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
@@ -1,11 +1,80 @@
 package ru.spbu.apcyb.svp.tasks;
 
-/**
- * Задание 4.
- */
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Task4 {
+    private static final Logger logger = LogManager.getLogger(Task4.class);
 
-  public static void main(String[] args) {
+    public static List<Double> getArgumentsListFromFile(String fileName) {
+        List<Double> argumentsList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String argumentsString;
+            double[] argumentsDoubleRow;
+            while ((argumentsString = bufferedReader.readLine()) != null) {
+                argumentsDoubleRow = Arrays.stream(argumentsString.trim().split("\\s+")).mapToDouble(Double::parseDouble).toArray();
+                for (double arg : argumentsDoubleRow)
+                    argumentsList.add(arg);
+            }
+        } catch (IOException e) {
+            logger.error("Ошибка!", e);
+        }
+        return argumentsList;
+    }
 
-  }
+    public static List<Double> singleThreadComputation(List<Double> argumentsList) {
+        List<Double> tanList = new ArrayList<>();
+        for (Double arguments : argumentsList) {
+            tanList.add(Math.tan(arguments));
+        }
+        return tanList;
+    }
+
+    public static List<Double> multiThreadComputation(List<Double> argumentsList, int numberOfThreads) throws IOException, ExecutionException {
+        if (numberOfThreads <= 0) {
+            throw new IOException("Количество потоков должно быть положительно!");
+        }
+        List<Double> tanList = new ArrayList<>();
+        List<Future<Double>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+        try {
+            for (int i = 0; i < argumentsList.size(); i++) {
+                int finalI = i;
+                Future<Double> values = executor.submit(() -> Math.tan(argumentsList.get(finalI)));
+                futures.add(values);
+            }
+        } finally {
+            executor.shutdown();
+        }
+        for (Future<Double> future : futures) {
+            try {
+                tanList.add(future.get());
+            } catch (Exception e) {
+                logger.error("Произошла ошибка!", e);
+            }
+        }
+        return tanList;
+    }
+
+    public static void writeToFile(String fileName, List<Double> argumentsList, List<Double> values) {
+        if (Files.exists(Path.of(fileName))) {
+            throw new IllegalArgumentException("Такой файл уже существует!");
+        }
+
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            for (int i = 0; i < values.size(); i++) {
+                fileWriter.write("x = " + argumentsList.get(i) + ", tan(x) =  " + values.get(i) + ";\n");
+            }
+        } catch (IOException e) {
+            logger.error("Произошла ошибка!", e);
+        }
+    }
+
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task4Test.java
@@ -1,8 +1,121 @@
 package ru.spbu.apcyb.svp.tasks;
 
-/**
- * Тесты для задания 4.
- */
-public class Task4Test {
+import org.junit.jupiter.api.Test;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class Task4Test {
+    private static final Logger logger = Logger.getLogger(Task4Test.class.getName());
+
+    private final String argumentFilePath = ".\\src\\test\\resources\\Test4\\Arguments.txt";
+    private final String existResultFilePath = ".\\src\\test\\resources\\Test4\\Result.txt";
+    private List<Double> arguments;
+
+    public void setUp(int amountOfNumbers) {
+        try (FileWriter fileWriter = new FileWriter(argumentFilePath)) {
+            for (int i = 0; i < amountOfNumbers; i++) {
+                fileWriter.write(i + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.arguments = Task4.getArgumentsListFromFile(argumentFilePath);
+    }
+
+    @Test
+    void testGetArgumentsListFromFile() {
+        List<Double> expected = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            expected.add((double) i);
+        }
+        setUp(10);
+        assertEquals(this.arguments, expected);
+    }
+
+    @Test
+    void testMultiThreadComputation() throws IOException, ExecutionException {
+        List<Double> expected = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            expected.add(Math.tan(i));
+        }
+        setUp(10);
+        List<Double> actual = Task4.multiThreadComputation(this.arguments, 10);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSingleThreadComputation() {
+        List<Double> expected = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            expected.add(Math.tan(i));
+        }
+        setUp(10);
+        List<Double> actual = Task4.singleThreadComputation(this.arguments);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testWriteToExistFile() {
+        assertThrows(IllegalArgumentException.class, () -> Task4.writeToFile(existResultFilePath, arguments, arguments));
+    }
+
+    @Test
+    void test10() throws IOException, ExecutionException, InterruptedException {
+        setUp(10);
+        long start, end;
+
+        start = System.currentTimeMillis();
+        Task4.singleThreadComputation(arguments);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N=10 в однопоточном режиме: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        Task4.multiThreadComputation(arguments, 10);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N=10 в многопоточном режиме: " + (end - start) + "ms");
+        assertEquals(Task4.singleThreadComputation(arguments), Task4.multiThreadComputation(arguments, 10));
+    }
+
+    @Test
+    void test100() throws IOException, ExecutionException, InterruptedException {
+        setUp(100);
+        long start, end;
+
+        start = System.currentTimeMillis();
+        Task4.singleThreadComputation(arguments);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N = 100 в однопоточном режиме: " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        Task4.multiThreadComputation(arguments, 10);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N=100 в многопоточном режиме: " + (end - start) + "ms");
+        assertEquals(Task4.singleThreadComputation(arguments), Task4.multiThreadComputation(arguments, 10));
+    }
+
+    @Test
+    void test1000000() throws IOException, ExecutionException, InterruptedException {
+        setUp(1000000);
+        long start, end;
+
+        start = System.currentTimeMillis();
+        Task4.singleThreadComputation(arguments);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N = 1000000 в однопоточном режиме " + (end - start) + "ms");
+
+        start = System.currentTimeMillis();
+        Task4.multiThreadComputation(arguments, 10);
+        end = System.currentTimeMillis();
+        logger.info("Время выполнения для N=1000000 в многопоточном режиме: " + (end - start) + "ms");
+        assertEquals(Task4.singleThreadComputation(arguments), Task4.multiThreadComputation(arguments, 10));
+    }
 }
