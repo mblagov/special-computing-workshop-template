865b0b3476c9bfb6f8ef72d6486346cab215c543
ZakirovRifkat <zakirov2323rifkat@gmail.com>
2023-03-26 20:35:35 +0300

    task-3

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task3.java
@@ -1,11 +1,46 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Задание 3.
  */
+
+
 public class Task3 {
 
-  public static void main(String[] args) {
+  /**
+   * Метод для вывода в файл файловго дерева.
+   *
+   * @param urlFile - путь к фалу/директории.
+   * @param writerFile - текстовый файл, в который выведется "дерево".
+   */
+  static boolean searchFiles(FileWriter writerFile, String urlFile) throws FileNotFoundException {
+    File firstFile = new File(urlFile);
+    File[] listOfFiles = firstFile.listFiles();
+    if (listOfFiles == null) {
+      throw new FileNotFoundException();
+    }
+    try {
+      for (File file : listOfFiles) {
+        if (file.isDirectory() && !file.isHidden()) {
+          writerFile.write(file.getPath() + "\n");
+          searchFiles(writerFile, file.getAbsolutePath());
+        } else if (file.isFile()) {
+          writerFile.write((file.getName()) + "\n");
+        }
+      }
+      return true;
+    } catch (IOException exception) {
+      throw new FileNotFoundException();
+    }
+  }
 
+  public static void main(String[] args) throws IOException {
+    FileWriter writerFile = new FileWriter("result.txt", false);
+    searchFiles(writerFile, "..//");
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -1,8 +1,31 @@
 package ru.spbu.apcyb.svp.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
 /**
  * Тесты для задания 3.
  */
-public class Task3Test {
+class Task3Test {
 
+  @Test
+  void test1() throws IOException {
+    FileWriter resFile = new FileWriter("res1.txt", false);
+    FileNotFoundException thrown =
+        assertThrows(FileNotFoundException.class, () -> Task3.searchFiles(resFile, "....."));
+    assertEquals("java.io.FileNotFoundException", thrown.toString());
+  }
+
+  @Test
+  void test2() throws IOException {
+    FileWriter resFile = new FileWriter("res2.txt", false);
+    boolean result = Task3.searchFiles(resFile, "..//");
+    assertTrue(result);
+  }
 }
