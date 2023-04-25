8de231831ecabed3d9a2351a06daffafbe2853d6
Evgeniy <st087790@student.spbu.ru>
2023-04-25 14:23:45 +0300

    change task#3

--- src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -3,9 +3,10 @@ package ru.spbu.apcyb.svp.tasks;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -16,34 +17,33 @@ import ru.spbu.apcyb.svp.tasks.task3.Task3;
  */
 @TestInstance(Lifecycle.PER_CLASS)
 class Task3Test {
-  String path;
-  String file;
-
-  @BeforeAll
-  void init() {
-    path = ".\\src";
-    file = ".\\src\\test\\resources\\task3FileResult.txt";
-  }
 
   @Test
   void test1Task3() throws IOException {
+    var path = ".\\src";
     Assertions.assertNotNull(Task3.checkCatalogue(Paths.get(path)));
   }
 
   @Test
   void test2Task3() throws IOException {
+    var path = ".\\src\\test\\resources\\task3TestFolder";
+    var file = ".\\src\\test\\resources\\task3FileResult.txt";
     Task3.getFileTree(path, file);
     var scanner = new Scanner(new FileReader(file));
-    String fileContent = "";
+    List<String> fileContent = new ArrayList<>();
     while (scanner.hasNextLine()) {
-      fileContent += scanner.nextLine();
+      fileContent.add(scanner.nextLine());
     }
-    var list = Task3.checkCatalogue(Paths.get(path));
-    String listContent = "";
-    for (var string : list) {
-      listContent += string;
+    var expected = new String[] {
+        path + "\\folder1",
+        path + "\\folder2",
+        path + "\\file1.txt",
+        path + "\\file2.txt",
+        path + "\\file3.txt",
+    };
+    for (int i = 0; i < expected.length; i++) {
+      Assertions.assertTrue(fileContent.contains(expected[i]));
     }
-    Assertions.assertEquals(fileContent, listContent);
     scanner.close();
   }
 }
