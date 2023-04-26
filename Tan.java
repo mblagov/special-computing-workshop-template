0b1a5a6d1ee513aba86cebf291e04226d0b1386f
dacenkoam <74965320+dacenkoam@users.noreply.github.com>
2023-04-26 18:04:11 +0300

    Update Tan.java

--- Tan.java
+++ Tan.java
@@ -1,60 +1,16 @@
 package ru.spbu.apcyb.svp.tasks;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
+import java.util.concurrent.Callable;
 
-public class Tan extends Thread {
+public class Tan implements Callable<Double> {
+  private final double data;
 
-  FileWriter outMulti;
-  private final int startLine;
-  private final int lines;
-  private final String input;
-
-  /**
-   * Конструктор.
-   *
-   * @param startLine первая линия для чтения из файла.
-   * @param lines     количество линий для чтения.
-   * @param outMulti  файл для вывода.
-   * @param input     файл для чтения значений в формате строки.
-   */
-  public Tan(int startLine, int lines, FileWriter outMulti, String input) {
-    this.startLine = startLine;
-    this.lines = lines;
-    this.outMulti = outMulti;
-    this.input = input;
+  Tan(double data) {
+    this.data = data;
   }
-  public void tanToFile() throws FileNotFoundException {
-    try (BufferedReader reader = new BufferedReader(new FileReader(input))) {
-      String line = "";
-      for (int i = 0; i < startLine; i++) {
-        line = reader.readLine();
 
-      }
-      outMulti.write(Math.tan(Double.parseDouble(line)) + "\n");
-      for (int j = 1; j < lines; j++) {
-
-        line = reader.readLine();
-
-        outMulti.write(Math.tan(Double.parseDouble(line)) + "\n");
-      }
-    } catch (IOException e) {
-      throw new FileNotFoundException("Ошибка чтения из файла в многопоточном режиме");
-    }
-  }
-  /**
-   * Исполняемая команда.
-   */
   @Override
-  public void run() {
-    try {
-      tanToFile();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public Double call() throws Exception {
+    return Math.tan(this.data);
   }
-
 }
