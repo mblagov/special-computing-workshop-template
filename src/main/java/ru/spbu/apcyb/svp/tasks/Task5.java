3c052dff8419f1a360ae041167ce483b328b68cf
petrunov-yuriy <30lbeerforyou@gmail.com>
2023-05-16 07:10:04 +0300

    Add task 5

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task5.java
@@ -3,9 +3,69 @@ package ru.spbu.apcyb.svp.tasks;
 /**
  * Задание 5.
  */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task5 {
 
-  public static void main(String[] args) {
+  private Map<String, Long> map;
 
+  public void readAndCount(Path path) throws IOException {
+    this.map = Arrays.stream(Files.readAllLines(path).toArray(new String[]{}))
+        .flatMap(line -> Arrays.stream(line.split("\\p{Punct}|[0-9]+|\t|\\s+"))
+            .map(word -> Normalizer
+                .normalize(word, Normalizer.Form.NFD)
+                .replaceAll("[^\\u0401\\u0451\\u0410-\\u044f|\\p{ASCII}]", ""))
+            .filter(item -> !item.isEmpty())
+            .map(String::toLowerCase))
+        .collect(Collectors.groupingBy(
+            Function.identity(), Collectors.counting()));
+  }
+
+  public String mapToString() {
+    return map.entrySet().stream()
+        .map(e -> e.getKey() + " = " + e.getValue())
+        .collect(Collectors.joining("\n"));
+  }
+
+  public void printCount(Path path) throws IOException {
+    Files.write(path, Collections.singleton(mapToString()), StandardOpenOption.APPEND);
+  }
+
+  public void parallelFileCount(Path dirPath) throws IOException {
+    ExecutorService executor = Executors.newFixedThreadPool(10);
+    ArrayList<CompletableFuture> list = new ArrayList<>();
+    for (var entry : this.map.entrySet()) {
+      long numOfCopies = entry.getValue();
+      Path newPath = dirPath.resolve(entry.getKey() + ".txt");
+      String word = entry.getKey();
+      for (int i = 0; i < numOfCopies; i++) {
+        list.add(CompletableFuture.runAsync(() -> {
+          try {
+            Files.write(newPath, Collections.singleton(word), StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }, executor));
+      }
+    }
+    CompletableFuture<String>[] a = new CompletableFuture[0];
+    Stream.of(list.toArray(a)).map(CompletableFuture::join).collect(Collectors.joining("\n"));
+    executor.shutdown();
   }
 }
--- src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task5Test.java
@@ -3,6 +3,71 @@ package ru.spbu.apcyb.svp.tasks;
 /**
  * Тесты для задания 5.
  */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Objects;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class Task5Test {
 
+  private static final Task5 proc = new Task5();
+  private static final String bookPath =
+      ".\\src\\test\\resources\\num5\\WarAndPeace.txt";
+  private static final String directoryPath =
+      ".\\src\\test\\resources\\num5\\WordFiles";
+  private static final String countPath =
+      ".\\src\\test\\resources\\num5\\Counts.txt";
+  private static final String smallTextPath =
+      ".\\src\\test\\resources\\num5\\SmallTextTest.txt";
+
+  @BeforeAll
+  static void init() {
+    for (File file : Objects.requireNonNull(Paths.get(directoryPath).toFile().listFiles())) {
+      if (!file.isDirectory()) {
+        file.delete();
+      }
+    }
+    try {
+      new PrintWriter(Paths.get(countPath).toString()).close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void readAndCountTest() {
+    try {
+      proc.readAndCount(Paths.get(smallTextPath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    String expected = "bar = 1\nfoo = 3\nbaz = 1";
+    assertEquals(expected, proc.mapToString());
+  }
+
+  @Test
+  public void bigTextTest() {
+    try {
+      proc.readAndCount(Paths.get(bookPath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      proc.printCount(Paths.get(countPath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      proc.parallelFileCount(Paths.get(directoryPath));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
