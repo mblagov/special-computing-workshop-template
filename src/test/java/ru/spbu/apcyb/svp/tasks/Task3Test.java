17e5d02dec6f6c6fc150d53529518e22e4efabb9
Adam <adamsonad9@gmail.com>
2023-12-09 21:02:39 +0300

    task 3 with tests

---
+++ src/test/java/ru/spbu/apcyb/svp/tasks/Task3Test.java
@@ -0,0 +1,42 @@
+package ru.spbu.apcyb.svp.tasks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.NotDirectoryException;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Task 3.
+ */
+class Task3Test {
+
+  @Test
+  void testNotExistingDirectoryInput() throws IOException {
+    String[] args = new String[2];
+    args[0] = "con";
+    args[1] = "answer.txt";
+    FileWriter writer = new FileWriter(args[1], false);
+    FileNotFoundException thrown = assertThrows(FileNotFoundException.class, () -> Task3.walk(args[0], writer, 0));
+    assertEquals("java.io.FileNotFoundException", thrown.toString());
+  }
+
+  @Test
+  void testNormalInput() throws IOException {
+    String[] args = new String[2];
+    args[0] = "..//";
+    args[1] = "answer.txt";
+    FileWriter writer = new FileWriter(args[1], false);
+    boolean isEmpty = true;
+    try {
+      Task3.walk(args[0], writer, 0);
+    } catch (IOException thrown) {
+      isEmpty = false;
+    }
+    assertTrue(isEmpty);
+  }
+}
